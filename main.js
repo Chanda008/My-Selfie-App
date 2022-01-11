f1b349b03de9b5fea33f5b0aa2ba332dbaa9f3d4
Chanda008 <83400660+Chanda008@users.noreply.github.com>
2022-01-11 16:20:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,43 @@ recognition.onresult = function run(event){
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML = content;
+    if(content == "take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+    }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking Your Selfie In 5 Seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        takeSnapshot();
+        save();
+    },5000);   
+}
+
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="selfie_img" src="' + data_uri + '">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_img").src;
+    link.href = image;
+    link.click();
 }
